ac7d937e43be511baa6d9a3cacbeca02bebf9c3b
Gurman Singh <95881451+GurmanSinghh@users.noreply.github.com>
2022-12-05 13:03:35 -0500

    First Commit

---
+++ Lab 4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            salesRecord();
+        }
+        
+        static void salesRecord()
+
+        {
+            Console.Write("How many sales did you make?: ");
+
+            CommissionEarned();
+        }
+
+        static void CommissionEarned()
+        {
+
+
+            double commissionMade;
+            int totalSales;
+
+
+            totalSales = Int32.Parse(Console.ReadLine());
+
+
+            if (totalSales >= 0 || totalSales <= 999)
+            {
+                commissionMade = totalSales * 0.03;
+            }
+
+            else if (totalSales <= 2999)
+            {
+                commissionMade = totalSales * 0.04;
+            }
+
+            else
+            {
+                commissionMade = totalSales * 0.05;
+            }
+
+
+            Console.WriteLine($"You Made {commissionMade:c} In Commissions.");
+
+        }
+    }
+    }
+
